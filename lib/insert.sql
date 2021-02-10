e54227b8140b6689e2945ba139172840cb80bf9c
Luke Gill <bagrad007@gmail.com>
2021-02-09 21:06:44 -0500

    commit tree c8ae69b4721859fb11489709062e7b4418542a4f

---
+++ lib/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES
+    ("Sword of Truth", 2, 3),
+    ("Sword of Truth", 2, 3);
+
+INSERT INTO books (year, series_id, title) VALUES
+    (2000, 3, "Fairies"),
+    (2000, 3, "Fairies"),
+    (2000, 3, "Fairies"),
+    (2000, 3, "Fairies"),
+    (2000, 3, "Fairies"),
+    (2000, 3, "Fairies");
+
+INSERT INTO characters (name, species, motto, author_id) VALUES
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1),
+    ("Elfador", "Human-dwarf", "When the going gets tough get butts", 1);
+
+INSERT INTO subgenres (name) VALUES
+    ("goblin fiction"),
+    ("goblin fiction");
+
+INSERT INTO authors (name) VALUES 
+    ("Ern randt"),
+    ("Ern randt");
+
+INSERT INTO character_books (book_id, character_id) VALUES
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4),
+    (1,4);
